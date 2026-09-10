da9e7a72c57332bcaf631feffbc7d9d8227263b1
kiarabartuccio <158498694+kiarabartuccio@users.noreply.github.com>
2026-09-10 09:55:28 -0400

    Finished half the lab

---
+++ Assets/Scripts/TrapController.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class TrapController : MonoBehaviour
+{
+    public float rotationSpeed = 100f;
+
+    void Update()
+    {
+        transform.Rotate(0, rotationSpeed * Time.deltaTime, 0);
+    }
+}
+
